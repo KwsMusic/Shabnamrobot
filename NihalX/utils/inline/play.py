45257907c3363d02f96a07c3fca9d9380b763e1d
ashishking2006 <134121306+ashishking2006@users.noreply.github.com>
2023-11-17 03:19:18 +0530

    Update play.py

--- NihalX/utils/inline/play.py
+++ NihalX/utils/inline/play.py
@@ -40,7 +40,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 callback_data=f"add_playlist {videoid}",
             ),
             InlineKeyboardButton(
-                text="🌹 sᴏᴜʀᴄᴇ 🌹", url=f"https://t.me/MUSICBOT_OWNER"
+                text="👑ᴏᴡɴᴇʀ👑", url=f"https://t.me/II_ASHISH_GUPTA_IIl"
             ),
         ],
         [
@@ -68,7 +68,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="🌹 sᴏᴜʀᴄᴇ 🌹", url=f"https://t.me/MUSICBOT_OWNER"),
+            InlineKeyboardButton(text="👑ᴏᴡɴᴇʀ👑", url=f"https://t.me/II_ASHISH_GUPTA_IIl"),
         ],
         [
             InlineKeyboardButton(
@@ -91,7 +91,7 @@ def stream_markup(_, videoid, chat_id):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="🌹 sᴏᴜʀᴄᴇ 🌹", url=f"https://t.me/MUSICBOT_OWNER"),
+            InlineKeyboardButton(text="👑ᴏᴡɴᴇʀ👑", url=f"https://t.me/II_ASHISH_GUPTA_IIl"),
         ],
         [
             InlineKeyboardButton(
