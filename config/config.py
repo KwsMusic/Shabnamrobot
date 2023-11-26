9f034301293d3340f13efdf0d8ff4f6a8d04c675
UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>
2023-11-26 18:37:01 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -45,7 +45,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", "https://t.me/MUSICBOT_OWNER")
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/BWF_MUSIC1")
+    "SUPPORT_GROUP", "https://t.me/ALL_QUIZ_TAME")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
